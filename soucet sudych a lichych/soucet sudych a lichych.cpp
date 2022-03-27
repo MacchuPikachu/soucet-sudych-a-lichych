ec74f9f26fc52e6987f21d669e2cc62aa0170159
Jan Macháček <jan.machacek.020@pslib.cz>
2022-03-27 21:09:30 +0200

    Přidejte soubory projektu.

---
+++ soucet sudych a lichych/soucet sudych a lichych.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+    int soucet_sudych(int cislo) {
+        int suda = 0;
+        for (int i = 0; i <= cislo; i += 2) {
+            suda += i;
+        }
+        return suda;
+    }
+    int soucet_lichych(int cislo) {
+        int licha = 1;
+        for (int i = -1; i <= cislo; i += 2) {
+            licha += i;
+        }
+        return licha;
+    }
+
+    int main(){
+        int cislo = 13;
+    printf("soucet sudych: %d\n", soucet_sudych(cislo));
+    printf("soucet lichych: %d\n", soucet_lichych(cislo));
+}
